b3f383105896703241f5f8bc8889555e431874df
SufyanAkram16 <sufyanakram1223@gmail.com>
2020-12-24 08:58:15 +0500

    program2

--- Program1.js
+++
@@ -1,7 +0,0 @@
-var Temperature = prompt ("Temperature in Celsuis") ;
-
-Temperature = Number(Temperature);
-
-var Result = 9 / 5 * Temperature + 32 ;
-
-alert ( "Temperature in Fahreheit " + Result) ; 
---
+++ Program2.js
@@ -0,0 +1,7 @@
+var heightInch = prompt("Enter Height in Inches");
+
+heightInch = Number(heightInch);
+
+var heightInCentimeters = 2.54 * heightInch ;
+
+console.log(heightInCentimeters);

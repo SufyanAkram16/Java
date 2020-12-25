f6790659d69ed22bb687b57c15fd2888ec5aa3c6
SufyanAkram16 <sufyanakram1223@gmail.com>
2020-12-25 11:05:02 +0500

    program04

---
+++ program4.js
@@ -0,0 +1,10 @@
+var input = prompt ("Enter a number");
+input = Number(input);
+input = input % 2 ;
+
+if (input === 0){
+    alert("Entered number is even");
+}
+else {
+    alert("Entered number is odd");
+}
